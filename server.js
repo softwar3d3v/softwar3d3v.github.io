34e468c9f8d7ef6c3c840be823c6ed8f647422a4
snhs-r3c0n <michaelvick80eagles@gmail.com>
2017-08-22 12:42:47 -0400

    Add more to server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app     = express();
+const server  = require('http').createServer(app);
+const io      = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
