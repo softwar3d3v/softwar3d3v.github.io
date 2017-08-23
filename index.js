b1fba503ede2314f1e284360319d808812e9ffe9
snhs-r3c0n <michaelvick80eagles@gmail.com>
2017-08-23 18:57:01 -0400

    Change port number.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
-var port    = process.env.PORT || 3000;
+var port    = 80;
 
 app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res){
@@ -13,6 +13,6 @@ io.on('connection', function(socket){
        io.emit('chat message', msg); 
     });
 });
-http.listen(port, 'https://snhs-r3c0nn.herokuapp.com/', function() {
+http.listen(port, function() {
     console.log('Listening on *:' + port);
 });
