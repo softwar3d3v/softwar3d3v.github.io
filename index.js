319a2a742ae7b3e0811d319930f9caa453d10712
snhs-r3c0n <michaelvick80eagles@gmail.com>
2017-08-23 19:03:12 -0400

    fixes

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
-var port    = 80;
+var port    = 8000;
 
 app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/chat.html');
+    res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
     socket.on('chat message', function(msg) {
