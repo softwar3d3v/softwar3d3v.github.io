8e152e44c77ec5cd87dff51bda843495b60815fa
Taylor <30904545+snhs-r3c0n@users.noreply.github.com>
2017-08-24 12:11:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
-var port    = 8000;
+var port    = 80;
+var profanity = require('profanity-middleware');
 
 app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res){
